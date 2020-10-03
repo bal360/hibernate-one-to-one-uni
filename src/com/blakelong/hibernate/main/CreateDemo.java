a668cdf207eec231e7eb77da34a2ffb0f6f0f3b3
Blake A. Long <blake.a.long@gmail.com>
2020-10-03 13:09:24 -0600

    CreateDemo and test association and save

---
+++ src/com/blakelong/hibernate/main/CreateDemo.java
@@ -0,0 +1,52 @@
+package com.blakelong.hibernate.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.blakelong.hibernate.entity.Instructor;
+import com.blakelong.hibernate.entity.InstructorDetail;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+		
+		// create factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Instructor.class)
+								.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create objects
+			
+			Instructor tempInstructor = 
+					new Instructor("Blake", "Long", "blake@email.com");
+			
+			InstructorDetail tempInstructorDetail = 
+					new InstructorDetail("http://www.blong.com/youtube", "BJJ");
+			
+			// associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the instructor
+			// *** this will ALSO save the InstructorDetail object ***
+			// because of CascadeType.ALL
+			session.save(tempInstructor);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done");
+		} finally {
+			factory.close();
+		}
+	}
+
+}
